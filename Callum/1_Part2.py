f2510a391a45864ebe9015c86c1de3d0d8ebb1c9
Callum Shipton <callumshipton@msn.com>
2021-12-01 21:02:39 +0000

    #1_Part1 - Callum
    - Fixed part 1, it was a stupid python type issue, they were strings not ints.
    - Added part 2, same approach as part 1

---
+++ Callum/1_Part2.py
@@ -0,0 +1,15 @@
+import itertools as it
+
+with open("./1_input.txt", "r") as inputFile:
+    readingsStr = inputFile.read().splitlines()
+    readings = map(int, readingsStr)
+    readingA, readingB, readingC = it.tee(readings, 3)
+    next(readingB, None)
+    next(readingC, None)
+    next(readingC, None)
+    readingTrips = zip(readingA, readingB, readingC)
+    averageThreeReadings = map(lambda trip: trip[0] + trip[1] + trip[2], readingTrips)
+    averagePairs = it.pairwise(averageThreeReadings)
+    increasingPairs = map(lambda pair : pair[1] > pair[0], averagePairs)
+    numOfIncreasingPairs = sum(increasingPairs)
+    print (numOfIncreasingPairs)
