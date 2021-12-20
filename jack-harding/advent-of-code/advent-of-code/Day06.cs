7707569466f91d19c4c1dea87cd928b9f0804370
jackdanielharding <jackdanielharding@gmail.com>
2021-12-20 16:38:08 +0000

    Added Jack's Day 6

---
+++ jack-harding/advent-of-code/advent-of-code/Day06.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace advent_of_code
+{
+    public class Day06
+    {
+        public static void Start()
+        {
+            string[] inputLines = File.ReadAllLines("Day06RealInput.txt");
+            string input = inputLines.First();
+            CalculateSpawn(input, 256);
+        }
+
+        public static void CalculateSpawn(string input, int days)
+        {
+            List<int> starterFish = new(input.Split(",").Select(int.Parse));
+
+            var fish = new Dictionary<int, double>()
+            {
+                 {0, 0},
+                 {1, 0},
+                 {2, 0},
+                 {3, 0},
+                 {4, 0},
+                 {5, 0},
+                 {6, 0},
+                 {7, 0},
+                 {8, 0}
+            };
+
+            foreach(int starter in starterFish)
+            {
+                fish[starter]++;
+            }
+
+            foreach (int day in Enumerable.Range(1, days))
+            {
+                double newFish = 0;
+
+                foreach (int key in fish.Keys)
+                {
+                    if(key == 0)
+                    {
+                        newFish = fish[key];
+                    } else
+                    {
+                        fish[key - 1] = fish[key];
+                        fish[key] = 0;
+                    }
+                }
+                fish[8] += newFish;
+                fish[6] += newFish;
+            }
+
+            Console.WriteLine($"{fish.Values.Sum()} fish in {days} days");
+        }
+    }
+}
